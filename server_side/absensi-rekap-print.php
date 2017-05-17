5942e064378a0548ad12538341a492aca2d14054
Gema Ulama Putra <cybermujahidz@gmail.com>
2017-05-18 02:23:26 +0700

    ben ketok akeh

---
+++ server_side/absensi-rekap-print.php
@@ -0,0 +1,50 @@
+<?php
+require_once('koneksi.php');
+$data=array();
+$q=mysqli_query($con,"select * from absensi where
+		NIS = '".$_GET['NIS']."' and
+		TglAbsensi >= '".($_GET['tahun']-1)."-07-01' and
+		TglAbsensi < '".$_GET['tahun']."-07-01'
+		order by TglAbsensi desc");
+if(mysqli_num_rows($q)==0){
+	$data=array('zeroresult'=>true);
+}elseif(mysqli_num_rows($q)<=50){
+	echo "<table>";
+	echo "
+	  <thead>
+			<tr>
+				<th>Bulan</th>
+				<th>Sakit</th>
+				<th>Ijin</th>
+				<th>Alpa</th>
+			</tr>
+		</thead>
+		<tbody>
+	";
+	while($qdata=mysqli_fetch_array($q)){
+
+		$bulan=substr($qdata['TglAbsensi'],0,7);
+
+		if(!isset($data[$bulan])){
+			$data[$bulan]=array(
+				'id'=>$bulan,
+				'bulan'=>date('M Y',strtotime($bulan."-01")),
+				'sakit'=>0,
+				'ijin'=>0,
+				'alpa'=>0,
+			);
+		}
+		if($qdata['Absensi']=='S'){
+			$data[$bulan]['sakit']++;
+		}elseif($qdata['Absensi']=='I'){
+			$data[$bulan]['ijin']++;
+		}elseif($qdata['Absensi']=='A'){
+			$data[$bulan]['alpa']++;
+		}
+	}
+}else{
+	$data=array('encumbered'=>true);
+}
+echo json_encode($data);
+
+?>
