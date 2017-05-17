e37b5348392e90b96859408c23172d13997c8b9c
Gema Ulama Putra <cybermujahidz@gmail.com>
2017-05-18 04:51:10 +0700

    tambah print rekap absensi

--- server_side/absensi-rekap-print.php
+++ server_side/absensi-rekap-print.php
@@ -9,7 +9,28 @@ $q=mysqli_query($con,"select * from absensi where
 if(mysqli_num_rows($q)==0){
 	$data=array('zeroresult'=>true);
 }elseif(mysqli_num_rows($q)<=50){
-	echo "<table>";
+	$siswa=mysqli_fetch_array(mysqli_query($con,"select * from siswa where NIS='".$_GET['NIS']."'"));
+	echo "
+		<table width=100%>
+			<tr>
+				<td colspan=4 class='tabletitle'><span >REKAPITULASI ABSENSI</span></td>
+			</tr>
+			<tr>
+				<td colspan=4 class='tabletitle'><span>SMK YADIKA BANGIL</span></td>
+			</tr>
+				<td colspan=4 class='tabletitle'></td>
+			</tr>
+			<tr>
+				<td>NIS</td><td>: ".$siswa['NIS']."</td>
+				<td>Tahun Ajaran</td><td>: ".($_GET['tahun']-1)."/".($_GET['tahun'])."</td>
+			</tr>
+			<tr>
+				<td>Nama Siswa</td><td>: ".$siswa['NamaSiswa']."</td>
+				<td></td><td></td>
+			</tr>
+		</table>
+	";
+	echo "<table class='print-table'>";
 	echo "
 	  <thead>
 			<tr>
@@ -22,9 +43,7 @@ if(mysqli_num_rows($q)==0){
 		<tbody>
 	";
 	while($qdata=mysqli_fetch_array($q)){
-
 		$bulan=substr($qdata['TglAbsensi'],0,7);
-
 		if(!isset($data[$bulan])){
 			$data[$bulan]=array(
 				'id'=>$bulan,
@@ -42,9 +61,61 @@ if(mysqli_num_rows($q)==0){
 			$data[$bulan]['alpa']++;
 		}
 	}
+	foreach($data as $item){
+			echo "
+					<tr>
+			";
+			echo "<td>".$item['bulan']."</td>";
+			echo "<td>".$item['sakit']."</td>";
+			echo "<td>".$item['ijin']."</td>";
+			echo "<td>".$item['alpa']."</td>";
+			echo "
+				</tr>
+			";
+	}
+	echo "
+		</tbody>
+	</table>
+	";
+	echo "
+	<table WIDTH=100%>
+		<tr>
+			<td>Mengetahui,</td>
+			<td></td>
+			<td></td>
+		<tr>
+			<td>Kepala Sekolah</td>
+			<td></td>
+			<td>Wali kelas</td>
+		<tr>
+			<td colspan=3></td>
+		</tr>
+		</tr>
+		<td><U>ROBBIANTO,S.KOM</U></td>
+		<td></td>
+		<td>.......................</td>
+		</tr>
+	</table>
+	";
 }else{
 	$data=array('encumbered'=>true);
 }
-echo json_encode($data);
+
 
 ?>
+<style>
+.print-table {
+	border-collapse:collapse;
+	width:100%;
+}
+.print-table th {
+	border:1px solid #000;
+}
+.print-table td {
+	border:1px solid #000;
+}
+.tabletitle {
+	font-size:16px;
+	text-align:center;
+}
+</style>
